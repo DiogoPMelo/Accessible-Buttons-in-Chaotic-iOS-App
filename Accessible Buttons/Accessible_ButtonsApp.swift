b61d4a372eb50d4f9ec56d04bf54a4a0735fddb6
DiogoPMelo <diogopmelo@icloud.com>
2025-11-18 10:27:22 +0000

    Initial Commit

---
+++ Accessible Buttons/Accessible_ButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accessible_ButtonsApp.swift
+//  Accessible Buttons
+//
+//  Created by Diogo Melo on 18/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Accessible_ButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
